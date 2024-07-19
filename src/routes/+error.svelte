949734cd7e7d4460789e8fdd365dac346a3423f5
Anthony Headley <agheadley@gmail.com>
2024-07-19 16:31:32 +0100

    a1

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<section>
+    <h3>Oops. It looks like this page is not available.</h3>
+    <p>{$page.error ? $page.error.message : ''}</p>
+</section>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,40 @@
+<script>
+	import '../simple.css';
+	import { page } from '$app/stores'; 
+	
+	import {UserIcon,SettingsIcon} from 'svelte-feather-icons';
+
+	let menu={
+		links:[
+			{txt:'Overview',path:'/'},
+			{txt:'Edit',path:'/edit'},
+			{txt:'Preferences',path:'/preferences'},
+
+		]
+	};
+</script>
+
+<div class="app">
+	<header>
+		<nav>
+			<a href='/' class={$page.url.pathname==='/' ? 'current' : ''}>Home</a>
+			<a href='/settings' class={$page.url.pathname==='/settings' ? 'current' : ''}><SettingsIcon size="16"/></a>
+				
+		</nav>
+		<h3>Verbum</h3>
+		<p>A svelte5 test app</p>
+	
+		
+	</header>
+	<main>
+		<slot />
+	</main>
+
+	<footer>
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style>
+
+</style>
